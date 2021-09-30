480d96ce65d28a281522c4ddcc96c9c328fd4578
Tatsuya-Inoue926 <t.i.ykts.926@ezweb.ne.jp>
2021-10-01 06:08:13 +0900

    fix

--- newproject/settings.py
+++ newproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from . import settings_local
+from . import local_settings
 import os
 import dj_database_url
 
